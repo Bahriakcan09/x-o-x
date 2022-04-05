3b45c10c9d67279b1c5cb6b8ef46517634a32d0e
akcan <akcan@DESKTOP-K3L4LJU>
2022-04-05 23:38:14 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace x_o_x
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label03 = new System.Windows.Forms.Label();
+            this.label02 = new System.Windows.Forms.Label();
+            this.label01 = new System.Windows.Forms.Label();
+            this.label05 = new System.Windows.Forms.Label();
+            this.label04 = new System.Windows.Forms.Label();
+            this.label06 = new System.Windows.Forms.Label();
+            this.label09 = new System.Windows.Forms.Label();
+            this.label07 = new System.Windows.Forms.Label();
+            this.label08 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label03
+            // 
+            this.label03.BackColor = System.Drawing.Color.IndianRed;
+            this.label03.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label03.Location = new System.Drawing.Point(360, 86);
+            this.label03.Name = "label03";
+            this.label03.Size = new System.Drawing.Size(150, 150);
+            this.label03.TabIndex = 4;
+            this.label03.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label03.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label02
+            // 
+            this.label02.BackColor = System.Drawing.Color.IndianRed;
+            this.label02.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label02.Location = new System.Drawing.Point(190, 86);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(150, 150);
+            this.label02.TabIndex = 5;
+            this.label02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label02.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label01
+            // 
+            this.label01.BackColor = System.Drawing.Color.IndianRed;
+            this.label01.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label01.Location = new System.Drawing.Point(20, 86);
+            this.label01.Name = "label01";
+            this.label01.Size = new System.Drawing.Size(150, 150);
+            this.label01.TabIndex = 6;
+            this.label01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label01.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label05
+            // 
+            this.label05.BackColor = System.Drawing.Color.IndianRed;
+            this.label05.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label05.Location = new System.Drawing.Point(190, 259);
+            this.label05.Name = "label05";
+            this.label05.Size = new System.Drawing.Size(150, 150);
+            this.label05.TabIndex = 7;
+            this.label05.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label05.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label04
+            // 
+            this.label04.BackColor = System.Drawing.Color.IndianRed;
+            this.label04.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label04.Location = new System.Drawing.Point(20, 259);
+            this.label04.Name = "label04";
+            this.label04.Size = new System.Drawing.Size(150, 150);
+            this.label04.TabIndex = 8;
+            this.label04.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label04.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label06
+            // 
+            this.label06.BackColor = System.Drawing.Color.IndianRed;
+            this.label06.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label06.Location = new System.Drawing.Point(360, 259);
+            this.label06.Name = "label06";
+            this.label06.Size = new System.Drawing.Size(150, 150);
+            this.label06.TabIndex = 9;
+            this.label06.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label06.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label09
+            // 
+            this.label09.BackColor = System.Drawing.Color.IndianRed;
+            this.label09.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label09.Location = new System.Drawing.Point(360, 435);
+            this.label09.Name = "label09";
+            this.label09.Size = new System.Drawing.Size(150, 150);
+            this.label09.TabIndex = 12;
+            this.label09.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label09.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label07
+            // 
+            this.label07.BackColor = System.Drawing.Color.IndianRed;
+            this.label07.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label07.Location = new System.Drawing.Point(20, 435);
+            this.label07.Name = "label07";
+            this.label07.Size = new System.Drawing.Size(150, 150);
+            this.label07.TabIndex = 11;
+            this.label07.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label07.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label08
+            // 
+            this.label08.BackColor = System.Drawing.Color.IndianRed;
+            this.label08.Font = new System.Drawing.Font("Cambria", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label08.Location = new System.Drawing.Point(190, 435);
+            this.label08.Name = "label08";
+            this.label08.Size = new System.Drawing.Size(150, 150);
+            this.label08.TabIndex = 10;
+            this.label08.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label08.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(19, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 75);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "X ->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(359, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 75);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "<- O";
+            // 
+            // scor
+            // 
+            this.scor.AutoSize = true;
+            this.scor.BackColor = System.Drawing.Color.Black;
+            this.scor.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scor.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.scor.Location = new System.Drawing.Point(203, 9);
+            this.scor.Name = "scor";
+            this.scor.Size = new System.Drawing.Size(125, 75);
+            this.scor.TabIndex = 15;
+            this.scor.Text = "0-0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(530, 600);
+            this.Controls.Add(this.scor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label09);
+            this.Controls.Add(this.label07);
+            this.Controls.Add(this.label08);
+            this.Controls.Add(this.label06);
+            this.Controls.Add(this.label04);
+            this.Controls.Add(this.label05);
+            this.Controls.Add(this.label01);
+            this.Controls.Add(this.label02);
+            this.Controls.Add(this.label03);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label03;
+        private System.Windows.Forms.Label label02;
+        private System.Windows.Forms.Label label01;
+        private System.Windows.Forms.Label label05;
+        private System.Windows.Forms.Label label04;
+        private System.Windows.Forms.Label label06;
+        private System.Windows.Forms.Label label09;
+        private System.Windows.Forms.Label label07;
+        private System.Windows.Forms.Label label08;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label scor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace x_o_x
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.Draggable(true);
+            InitializeComponent();
+        }
+
+        int sayac = 0;
+        string last = "O";
+        private void x_yerlestir(object sender, EventArgs e)
+        {
+            Label a = sender as Label;
+            if (last == "X") return;
+            if (a.Text.Equals("O") || a.Text.Equals("X"))
+            {
+                return;
+            }
+            a.Text = "X";
+            last = "X";
+            sayac++;
+            kontrol("X");
+        }
+        private void o_yerlestir(object sender, EventArgs e)
+        {
+            Label a = sender as Label;
+            if (last == "O") return;
+            if (a.Text.Equals("O")|| a.Text.Equals("X"))
+            {
+                return;
+            }
+            a.Text = "O";
+            last = "O";
+            sayac++;
+            kontrol("O");
+        }
+        private void m_click(object sender, MouseEventArgs e)
+        {
+            Label a = sender as Label;
+            if(e.Button == MouseButtons.Left)
+            {
+                if(last == "X")
+                {
+                    o_yerlestir(a,e);
+                }
+                else
+                {
+                    x_yerlestir(a, e);
+                }
+            }
+        }
+        public void kontrol(string str)
+        {
+            if (label01.Text == str && label02.Text == str && label03.Text == str) win(str);
+            if (label04.Text == str && label05.Text == str && label06.Text == str) win(str);
+            if (label07.Text == str && label08.Text == str && label09.Text == str) win(str);
+                                                                                      
+            if (label01.Text == str && label04.Text == str && label07.Text == str) win(str);
+            if (label02.Text == str && label05.Text == str && label08.Text == str) win(str);
+            if (label03.Text == str && label06.Text == str && label09.Text == str) win(str);
+                                                                                       
+            if (label07.Text == str && label05.Text == str && label03.Text == str) win(str);
+            if (label01.Text == str && label05.Text == str && label09.Text == str) win(str);
+
+            if (sayac == 9) restrart();
+        }
+        int x, o;
+        public void win(string s)
+        {
+            MessageBox.Show(s + " KAZANDI");
+            if (s == "X")
+            {
+                x++;
+            }
+            else if (s == "O")
+            {
+                o++;
+            }
+            if (o == 3 || x == 3)
+            {
+                x = 0;
+                o=0;
+            }
+            scor.Text = x+"-"+o;
+            restrart();
+        }  
+        public void restrart()
+        {
+            label01.Text = "";
+            label02.Text = "";
+            label03.Text = "";
+            label04.Text = "";
+            label05.Text = "";
+            label06.Text = "";
+            label07.Text = "";
+            label08.Text = "";
+            label09.Text = "";
+            sayac = 0;
+        }
+    }
+}
